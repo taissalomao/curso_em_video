caf6d5c2a6160a1bcdfeeb7e1816f0013088244e
taissalomao <tais.salomao@fatec.sp.gov.br>
2021-10-03 20:31:58 -0300

    exercício 03.

---
+++ curso_em_video/exercicio_003.py
@@ -0,0 +1,8 @@
+# Crie um programa que leia dois números e mostre a soma entre eles.
+
+num1 = int(input("Digite um número "))
+num2 = int(input("Digite mais um número "))
+
+soma = num1 + num2
+
+print(f'A soma entre {num1} e {num2} é {soma}')
