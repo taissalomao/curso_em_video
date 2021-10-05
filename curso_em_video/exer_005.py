a162017a5b45c16b513d1ea85363a9c7c0fed3ec
taissalomao <tais.salomao@fatec.sp.gov.br>
2021-10-05 10:58:00 -0300

    exercicio cinco

---
+++ curso_em_video/exer_005.py
@@ -0,0 +1,6 @@
+# Faça um programa que leia um número inteiro e mostre na tela seu sucessor e seu antecessor.
+
+num = int(input("digite um número "))
+antecessor = num - 1
+sucessor = num + 1
+print(f'Analisando o valor {num}, seu antecessor é {antecessor} e seu sucessor é {sucessor}')
