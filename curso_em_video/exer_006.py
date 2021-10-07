a47f130ec6e1c1df96d06091dd0d808250b5d7fc
taissalomao <tais.salomao@fatec.sp.gov.br>
2021-10-06 22:51:58 -0300

    exercicio seis

---
+++ curso_em_video/exer_006.py
@@ -0,0 +1,8 @@
+# Crie um algoritmo que leia um npumero e mostre o seu dobro, triplo e sua raiz quadrada.
+
+num = int(input('Digite um número '))
+0
+dobro = num * 2
+triplo = num * 3
+raiz= num ** 0.5
+print(f'vc digitou {num} seu dobro é {dobro}, o triplo {triplo} e sua raiz é {raiz}')
