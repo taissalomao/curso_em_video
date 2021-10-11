e89e55c8475b6317fa03a5d06ac07fd9855dc2f2
taissalomao <tais.salomao@fatec.sp.gov.br>
2021-10-10 23:18:22 -0300

    exercicio oito

---
+++ curso_em_video/exer_008.py
@@ -0,0 +1,8 @@
+# Escreva um programa que leia um valor em metros e o exiba convertido em centimetros e milimetros.
+
+a = int(input("digite um valor em metros "))
+
+b = a * 100
+c = a * 1000
+
+print(f'{a} convertido em centimetros equivale a {b} e em milimetros equivale a {c}')
