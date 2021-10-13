66e5699186e29cee803905c729fbcc620f41dd16
taissalomao <tais.salomao@fatec.sp.gov.br>
2021-10-12 23:27:29 -0300

    exercicio quatorze

---
+++ exer_014.py
@@ -0,0 +1,7 @@
+# Escreva um programa que converta uma temperatura digitada em C° para F°
+
+temp = float(input("Digite uma temperatura em celsius "))
+
+tempConvertida = (temp * 1.8) + 32
+
+print(f'{temp} convertida para Fahrenheit é {tempConvertida}')
